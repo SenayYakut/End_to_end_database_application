6f6ea8e248bb0144b37cbbea67fbb96778e2b1ff
Senay Yakut <senaykt@gmail.com>
2019-02-14 13:42:25 -0800

    Add files via upload

---
+++ auction_app/config.py
@@ -0,0 +1,2 @@
+
+secret_key= "SOME LONG RANDOM STRING HERE"
---
+++ auction_app/create_app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+import config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile("config.py")
+    app.secret_key = config.secret_key
+
+
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+     #from users import user_blueprint
+     #app.register_blueprint(user_blueprint, url_prefix="/users")
+
+    return app
+
+if __name__ == "__main__":
+    import os
+    app = create_app()
+    app.run(port=int(os.getenv("PORT",8080)), host=os.getenv("IP", "127.0.0.1"), debug=True)        
